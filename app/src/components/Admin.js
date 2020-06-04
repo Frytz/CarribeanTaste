478b78155f96e35bd200ed07305071c7d30fedd8
Brian Clayton <finalb@projectfinalboss.com>
2020-06-03 20:54:18 -0400

    Adding cms details

---
+++ app/src/components/Admin.js
@@ -0,0 +1,7 @@
+import {createClient} from 'contentful';
+
+export default createClient ({
+    space: "th2oo60bbjti",
+    accessToken: "Um5mVxr-fEQ7fbCsKuggsfhBBbAkfYuCgpdir5IEOrs"
+})
+
--- app/src/contexts/activeContent.js
+++ app/src/contexts/activeContent.js
@@ -1,3 +1,20 @@
-import React, {createContext} from 'react';
+import React, {createContext, Component} from 'react';
+import Client from '../components/Admin'
+
 
 export const activeContContext = createContext();
+
+class ProductProvider extends Component {
+
+    state= {
+
+        products: [],
+        category: [],
+        tags: [],
+        recipes: [],
+        price: 0,
+        isSoldOut: false,
+
+    }
+
+}
